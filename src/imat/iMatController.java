f1cb1ec82e9a3ff6afa793b808f92d196503bf27
GIFF <Per_a99@hotmail.com>
2019-05-14 10:58:36 +0200

    starter

---
+++ src/imat/iMatController.java
@@ -0,0 +1,57 @@
+package imat;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TextField;
+
+import javax.swing.text.html.ImageView;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ResourceBundle;
+
+public class iMatController implements Initializable {
+
+    @FXML
+    TextField searchBar;
+
+
+
+
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+
+    void emptyCart(){
+        //töm en lista som har varor i sig?
+    }
+    void showFavorites(){
+        //sätt favoritvyn längst fram
+    }
+    void closeFavorites(){
+        //sätt favoritvyn längstbak
+    }
+    void showCheckOut(){
+        //sätt kassan länngst fram
+
+    }
+    void closeCheckOut(){
+        //sätt kassan längst bak
+    }
+    void searchItem(){
+
+
+        //metod då man klickat att man sökt som visar sina varor
+        getSearchedItems();
+    }
+    ArrayList getSearchedItems(){
+        return null;
+    }
+
+
+
+
+
+}
